6a7e1377b41092ebabca15296e67ba5d3512b1ec
tqp <timothyqpowers@gmail.com>
2022-02-07 11:57:55 -0500

    Added scheduler example

---
+++ scheduler.py
@@ -0,0 +1,29 @@
+import time
+
+import requests
+import schedule
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+data = requests.get(url)
+parameters = data.json()
+
+
+def fetch_bitcoin():
+    print("Getting Bitcoin Price")
+    result = parameters['bpi']['USD']
+    print(result)
+
+
+def fetch_bitcoin_by_currency(x):
+    print("Getting bitcoin price in: ", x)
+    result = parameters['bpi'][x]
+    print(result)
+
+
+# time
+schedule.every(4).seconds.do(fetch_bitcoin)
+schedule.every(7).seconds.do(fetch_bitcoin_by_currency, 'GBP')
+schedule.every(9).seconds.do(fetch_bitcoin_by_currency, 'EUR')
+while True:
+    schedule.run_pending()
+    time.sleep(1)
